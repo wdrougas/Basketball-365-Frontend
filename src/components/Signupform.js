f47ea4029c3960535056f4de84db4084bb52ae71
Will Drougas <wdrougas@gmail.com>
2020-02-03 14:31:02 -0500

    favorite team dropdown working

--- src/components/Signupform.js
+++ src/components/Signupform.js
@@ -1,31 +1,48 @@
 import React from 'react'
-import {Form} from 'semantic-ui-react'
+import {Form, Button} from 'semantic-ui-react'
+import {connect} from 'react-redux'
 
 
-const Signupform = props => {
+class Signupform extends React.Component {
+  constructor() {
+    super()
+  }
+
+
+handleSubmit = e => {
+  console.log(e)
+  }
+
+  
+  render() {
     return (
     <div> 
         <br/>
         <br/>
         <br/>
-    <Form>
+    <Form onSubmit={this.handleSubmit}>
     <Form.Group widths='equal'>
       <Form.Field label='First Name' placeholder='First Name' control='input' />
       <Form.Field label='Last Name' placeholder='Last Name' control='input' />
       <Form.Field label='Username' placeholder='Username' control='input' />
       <Form.Field label='Email' placeholder='Email' control='input' />
-      <Form.Field label='Password' placeholder='Password' control='input' />
+      <Form.Field type='password' label='Password' placeholder='Password' control='input' />
       <Form.Field label='Favorite Team' control='select'>
-        <option value='male'>Male</option>
-        <option value='female'>Female</option>
+        {this.props.teams.map(team => <option key={team.id} value={`${team.id}`}>{team.name}</option>)}
       </Form.Field>
-      <Form.Field label='Create Profile' control='button'>
-      Submit
-    </Form.Field>
+      <Button type='submit'>Submit</Button>
     </Form.Group>
   </Form>
-</div>
+  </div>
     )
+  }
+
 }
 
-export default Signupform
+
+const mapStateToProps = state => {
+    return {
+      teams: state.teams
+    }
+}
+export default connect(mapStateToProps)(Signupform)
