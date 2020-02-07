725281e4e4b939fb8df31c2474d56a7eb153cedc
Your Name <you@example.com>
2020-02-07 11:03:36 -0500

    user can edit favorite from profile page

--- src/components/TeamDetail.js
+++ src/components/TeamDetail.js
@@ -12,107 +12,104 @@ const favoritesData = 'http://localhost:3000/favorites'
 class TeamDetail extends React.Component {
 
 
-    createFavorite = (player) => { 
-        let playerId = player.id      
-        let configOptions = {
-            method: "POST",
-            headers: {
-                "Accept": 'application/json',
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify({
-                user_id: this.props.user.id,
-                player_id: playerId
-            })
-        }
-        fetch(favoritesData, configOptions)
-        .then(res => res.json())
-      .then(data => {
-          if (data.message === "Player added to favorites!") {
-              let newObj = JSON.parse(data.favorite)
-              this.props.addedFavorite(newObj)
-              swal("Done!", data.message, "success")
-          } else {
-              swal("Error!", data.message, 'error')
-          }
-      })
-      .catch(error => alert(error.message))
-    }
-
-
-    removeFavorite = (favorite) => {
-        fetch(favoritesData + `/${favorite.id}`, {
-            method: "DELETE"
+createFavorite = (player) => { 
+    let playerId = player.id      
+    let configOptions = {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: this.props.user.id,
+            player_id: playerId
         })
-        .then(res => res.json())
-        .then(favorite => this.props.deleteFavorite(favorite))
     }
+    fetch(favoritesData, configOptions)
+    .then(res => res.json())
+  .then(data => {
+      if (data.message === "Player added to favorites!") {
+          let newObj = JSON.parse(data.favorite)
+          this.props.addedFavorite(newObj)
+          swal("Done!", data.message, "success")
+      } else {
+          swal("Error!", data.message, 'error')
+      }
+  })
+  .catch(error => alert(error.message))
+}
+   removeFavorite = (favorite) => {
+    fetch(favoritesData + `/${favorite.id}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(favorite => this.props.deleteFavorite(favorite))
+}
 
     
-    render() {
-    var games = this.props.team.home_games.concat(this.props.team.visiting_games)
-    var sorted = games.sort((a,b) => {return Date.parse(a.date) - Date.parse(b.date)})
-    console.log(games)
-        return  !this.props.team ? <div className="ui active transition visible dimmer">
-        <div className="content"><div className="ui text loader">Loading</div></div>
-      </div> : (
-            <div>
+render() {
+var games = this.props.team.home_games.concat(this.props.team.visiting_games)
+var sorted = games.sort((a,b) => {return Date.parse(a.date) - Date.parse(b.date)})
+console.log(games)
+    return  !this.props.team ? <div className="ui active transition visible dimmer">
+    <div className="content"><div className="ui text loader">Loading</div></div>
+  </div> : (
+        <div>
+            <br/>
+            <br/>
+            <br />
+            <h1>{this.props.team.name}</h1>
+            <Segment>
+            <Grid columns ={2} relaxed='very'>
+            <Grid.Column>
+            <h3>Roster</h3>
+            {this.props.team.players.map(player => {
+            return <List key={player.id} player={player} >
+            <List.Item >
+                <Modal trigger={<List.Header as='a'>{player.position} - {player.first_name} {player.last_name}</List.Header>}>
+                    <Modal.Header>
+                        <p>{player.first_name} {player.last_name}
+                        {this.props.user && !this.props.user.favorites.map(favorite => favorite.player_id).includes(player.id) ? <Button size='mini' onClick={() => this.createFavorite(player)}>Add To Favorites</Button> : <Button size='mini' onClick={() => this.removeFavorite(this.props.favorites.find(favorite => favorite.user_id === this.props.user.id && favorite.player_id === player.id ))}>Remove From Favorites</Button>}
+                        
+                         </p>
+                        </Modal.Header>
+                    <Modal.Content>
+                        <p>College: {player.college}</p>
+                        <p>Years Pro: {player.yearsPro}</p>
+                        <p>Date of Birth: {player.date_of_birth}</p>
+                        <p>Country: {player.country} </p>
+                    </Modal.Content>
+               </Modal>               
+                </List.Item>
+                </List>})}
                 <br/>
                 <br/>
-                <br />
-                <h1>{this.props.team.name}</h1>
-                <Segment>
-                <Grid columns ={2} relaxed='very'>
+                <CommentContainer team={this.props.team}/>
+            </Grid.Column>
+            <Grid.Column>
+                <h3>Schedule</h3>
+                {sorted.map(game => {
+                    return <List key={game.id} game={game}>
+                        <List.Item>
+                            <Image avatar src={game.visiting_team_logo} />
+                            <List.Content>
+                <List.Header as='a'><Link to={`/teams/${game.visiting_team_id}`}>{game.visiting_team_name}</Link> {game.visiting_team_score} | {game.home_team_score} {game.home_team_name} </List.Header>
+                           </List.Content>
+                        </List.Item>
+                    </List>
+                })}
+            </Grid.Column>
+            </Grid>
+            {/* <Grid centered columns={2}>
                 <Grid.Column>
-                <h3>Roster</h3>
-                {this.props.team.players.map(player => {
-                return <List key={player.id} player={player} >
-                <List.Item >
-                    <Modal trigger={<List.Header as='a'>{player.position} - {player.first_name} {player.last_name}</List.Header>}>
-                        <Modal.Header>
-                            <p>{player.first_name} {player.last_name}
-
-                            {this.props.user && !this.props.user.favorites.map(favorite => favorite.player_id).includes(player.id) ? <Button size='mini' onClick={() => this.createFavorite(player)}>Add To Favorites</Button> : <Button size='mini' onClick={() => this.removeFavorite(this.props.favorites.find(favorite => favorite.user_id === this.props.user.id && favorite.player_id === player.id ))}>Remove From Favorites</Button>}
-                            
-                             </p>
-                            </Modal.Header>
-                        <Modal.Content>
-                            <p>College: {player.college}</p>
-                            <p>Years Pro: {player.yearsPro}</p>
-                            <p>Date of Birth: {player.date_of_birth}</p>
-                            <p>Country: {player.country} </p>
-                        </Modal.Content>
-                    </Modal>               
-                    </List.Item>
-                    </List>})}
-                    <br/>
-                    <br/>
                     <CommentContainer team={this.props.team}/>
                 </Grid.Column>
-                <Grid.Column>
-                    <h3>Schedule</h3>
-                    {sorted.map(game => {
-                        return <List key={game.id} game={game}>
-                            <List.Item>
-                                <Image avatar src={game.visiting_team_logo} />
-                                <List.Content>
-                    <List.Header as='a'><Link to={`/teams/${game.visiting_team_id}`}>{game.visiting_team_name}</Link> {game.visiting_team_score} | {game.home_team_score} {game.home_team_name} </List.Header>
-                                </List.Content>
-                            </List.Item>
-                        </List>
-                    })}
-                </Grid.Column>
-                </Grid>
-                {/* <Grid centered columns={2}>
-                    <Grid.Column>
-                        <CommentContainer team={this.props.team}/>
-                    </Grid.Column>
-                </Grid> */}
-                </Segment>
-            </div>
-            )
-        }
+            </Grid> */}
+            </Segment>
+        </div>
+        )
     }
+}
 
 
 const mapDispatchToProps = dispatch => {
@@ -135,7 +132,6 @@ const mapStateToProps = (store, ownProps) => {
         favorites: store.favorites
     }
 }
-
 
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TeamDetail))
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Grid, Header, List, Modal, Button} from 'semantic-ui-react'
+import {addedFavorite, deleteFavorite} from '../redux/actionCreators'
 
 
+
+const favoritesData = 'http://localhost:3000/favorites'
 class UserProfile extends React.Component {
+
+removeFavorite = (favorite) => {
+    fetch(favoritesData + `/${favorite.id}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(favorite => this.props.deleteFavorite(favorite))
+}
+
     render() {
         return (
             <div>
@@ -10,6 +23,40 @@ class UserProfile extends React.Component {
                 <br/>
                 <br/>
             <h1>{this.props.user.first_name} {this.props.user.last_name}</h1>
+            <Grid columns={2} relaxed='very'>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Header>Profile Information</Header>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Header>Favorite Players</Header>
+                        {this.props.user.favorites.map(favorite => {
+                         return <List key={favorite.player.id} player={favorite.player} >
+                         <List.Item >
+                             <Modal trigger={<List.Header as='a'>{favorite.player.position} - {favorite.player.first_name} {favorite.player.last_name}</List.Header>}>
+                                 <Modal.Header>
+                                     <p>{favorite.player.first_name} {favorite.player.last_name}        
+                                     <Button size='mini' onClick={() => this.removeFavorite(this.props.favorites.find(favorite => favorite.user_id === this.props.user.id && favorite.player_id === favorite.player.id ))}>Remove From Favorites</Button>
+                                      </p>
+                                     </Modal.Header>
+                                 <Modal.Content>
+                                     <p>College: {favorite.player.college}</p>
+                                     <p>Years Pro: {favorite.player.yearsPro}</p>
+                                     <p>Date of Birth: {favorite.player.date_of_birth}</p>
+                                     <p>Country: {favorite.player.country} </p>
+                                 </Modal.Content>
+                             </Modal>               
+                             </List.Item>
+                             </List>   
+                        })}
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Header>Team</Header>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
             </div>
         )
     }
@@ -18,9 +65,18 @@ class UserProfile extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        user: state.currentUser
+        user: state.currentUser,
+        favorites: state.favorites
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        deleteFavorite: (favorite) => {
+            dispatch(deleteFavorite(favorite))
+        }
     }
 }
 
 
-export default connect(mapStateToProps)(UserProfile)
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile)
